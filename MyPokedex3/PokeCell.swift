d08fa2dfa4fe4b45eea63827d6c92aebca9a5866
daxapps <daxapps777@gmail.com>
2017-02-08 08:21:25 -0600

    Add custom cell Pokemon

---
+++ MyPokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  MyPokedex3
+//
+//  Created by Jason Crawford on 2/8/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
