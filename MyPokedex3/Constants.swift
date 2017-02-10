7b07009bb909a85e2b46298da05a9e0c109fe5cb
daxapps <daxapps777@gmail.com>
2017-02-10 14:57:39 -0600

    Pulled info from Pokemon API to populate table on DetailVC

---
+++ MyPokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MyPokedex3
+//
+//  Created by Jason Crawford on 2/10/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
